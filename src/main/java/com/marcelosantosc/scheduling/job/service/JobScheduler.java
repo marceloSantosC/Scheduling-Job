c5ac79bcfbd8b87e08f161972a089301627c3e6c
Marcelo dos Santos <marcelo17082000@gmail.com>
2021-12-17 20:49:53 -0300

    + Adicionado método de validação para as jobs

--- src/main/java/com/marcelosantosc/scheduling/job/service/JobScheduler.java
+++ src/main/java/com/marcelosantosc/scheduling/job/service/JobScheduler.java
@@ -1,13 +1,50 @@
 package com.marcelosantosc.scheduling.job.service;
 
 import com.marcelosantosc.scheduling.job.model.Job;
+import com.marcelosantosc.scheduling.job.model.ValidationResult;
 
-import java.util.*;
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class JobScheduler {
 
-    public Queue<Job> sortJobsByDate(List<Job> jobs) {
+    private static final Duration MAX_JOB_DURATION = Duration.ofHours(8);
+
+
+    public ValidationResult validateJobs(LocalDateTime windowStart,
+                                         LocalDateTime windowEnd,
+                                         List<Job> jobs) {
+
+        ValidationResult validationResult = new ValidationResult();
+        validationResult.addValidation(!isJobDurationGreaterThanMax(jobs), "Existem jobs com mais de 8 horas de tempo estimado");
+        validationResult.addValidation(!aretherejobsoutoftheexecutionwindow(jobs, windowStart, windowEnd),
+                "Existem jobs com o prazo de execução fora janela de execução");
+        validationResult.addValidation(!executionTimeGreaterThanJobWindow(jobs, windowStart, windowEnd),
+                "O tempo de execução das jobs é maior do que a duração da janela de execução");
+
+        return validationResult;
+    }
+
+    private boolean isJobDurationGreaterThanMax(List<Job> jobs) {
+        return jobs.stream().anyMatch(job -> job.getEstimatedTime().compareTo((MAX_JOB_DURATION)) > 0);
+
+    }
+
+    private boolean aretherejobsoutoftheexecutionwindow(List<Job> jobs, LocalDateTime windowStart, LocalDateTime windowEnd) {
+        return jobs.stream().anyMatch(job ->
+                job.getDeadlineForExecution().isAfter(windowEnd) || job.getDeadlineForExecution().isBefore(windowStart));
+    }
+
+    private boolean executionTimeGreaterThanJobWindow(List<Job> jobs, LocalDateTime windowStart, LocalDateTime windowEnd) {
+        Duration executionDuration = jobs.stream().map(Job::getEstimatedTime).reduce(Duration.ZERO, Duration::plus);
+        return windowStart.plus(executionDuration).isAfter(windowEnd);
+    }
+
+    public LinkedList<Job> sortJobsByDate(List<Job> jobs) {
         return jobs.stream()
                 .sorted(Comparator.comparing(Job::getDeadlineForExecution))
                 .collect(Collectors.toCollection(LinkedList::new));
--- src/test/java/com/marcelosantosc/service/JobSchedulerTest.java
+++ src/test/java/com/marcelosantosc/service/JobSchedulerTest.java
@@ -1,6 +1,7 @@
 package com.marcelosantosc.service;
 
 import com.marcelosantosc.scheduling.job.model.Job;
+import com.marcelosantosc.scheduling.job.model.ValidationResult;
 import com.marcelosantosc.scheduling.job.service.JobScheduler;
 import org.junit.jupiter.api.Test;
 
@@ -42,24 +43,93 @@ public class JobSchedulerTest {
     }
 
     @Test
-    void should_not_sort_jobs_by_date() {
-        Job firstJob = newJob(LocalDateTime.parse("2021-10-12T10:20:30"), Duration.ofHours(2));
-        Job secondJob = newJob(LocalDateTime.parse("2021-10-13T12:30:00"), Duration.ofHours(4));
-        Job thirdJob = newJob(LocalDateTime.parse("2021-11-25T14:15:00"), Duration.ofHours(6));
-        Job lastJob =  newJob(LocalDateTime.parse("2021-12-15T14:30:00"), Duration.ofHours(8));
+    void should_not_return_validation_errors_with_valid_jobs() {
+        Job firstJob = newJob(LocalDateTime.parse("2020-10-12T10:20:30"), Duration.ofHours(2));
+        Job secondJob = newJob(LocalDateTime.parse("2020-10-13T12:30:00"), Duration.ofHours(6));
+        Job thirdJob = newJob(LocalDateTime.parse("2020-10-12T11:20:30"), Duration.ofHours(8));
 
-        List<Job> sortedList = new LinkedList<>();
-        sortedList.add(firstJob);
-        sortedList.add(secondJob);
-        sortedList.add(thirdJob);
-        sortedList.add(lastJob);
+        LocalDateTime executionWindowStart = LocalDateTime.parse("2020-10-12T10:20:30");
+        LocalDateTime executionWindowEnd = LocalDateTime.parse("2020-10-20T11:20:30");
 
-        List<Job> jobsToSort = new ArrayList<>(sortedList);
-        Collections.shuffle(jobsToSort);
+        List<Job> jobs = Arrays.asList(firstJob, secondJob, thirdJob);
 
-        Queue<Job> jobsSortedByAlgorithm = jobScheduler.sortJobsByDate(jobsToSort);
+        ValidationResult validationResult = jobScheduler.validateJobs(executionWindowStart, executionWindowEnd, jobs);
 
-        assertEquals(sortedList, jobsSortedByAlgorithm);
+        assertTrue(validationResult.isValid());
+        assertTrue(validationResult.getMessages().isEmpty());
+    }
+
+    @Test
+    void list_with_jobs_after_execution_window_end_should_be_invalid() {
+
+        Job firstJob = newJob(LocalDateTime.parse("2020-10-12T10:20:30"), Duration.ofHours(1));
+        Job secondJob = newJob(LocalDateTime.parse("2021-10-13T12:30:00"), Duration.ofHours(1));
+        Job thirdJob = newJob(LocalDateTime.parse("2019-10-12T00:20:30"), Duration.ofHours(1));
+
+        LocalDateTime executionWindowStart = LocalDateTime.parse("2019-10-12T00:20:30");
+        LocalDateTime executionWindowEnd = LocalDateTime.parse("2020-10-12T14:20:00");
+
+        List<Job> jobs = Arrays.asList(firstJob, secondJob, thirdJob);
+
+        ValidationResult validationResult = jobScheduler.validateJobs(executionWindowStart, executionWindowEnd, jobs);
+
+        assertFalse(validationResult.isValid());
+        assertEquals( 1, validationResult.getMessages().size());
+        assertEquals(validationResult.getMessages().get(0), "Existem jobs com o prazo de execução fora janela de execução");
+    }
+
+    @Test
+    void list_with_jobs_before_execution_window_end_should_be_invalid() {
+
+        Job firstJob = newJob(LocalDateTime.parse("2020-10-12T10:20:30"), Duration.ofHours(1));
+        Job secondJob = newJob(LocalDateTime.parse("2021-10-13T12:30:00"), Duration.ofHours(1));
+        Job thirdJob = newJob(LocalDateTime.parse("2019-10-12T00:20:30"), Duration.ofHours(1));
+
+        LocalDateTime executionWindowStart = LocalDateTime.parse("2022-12-17T00:20:30");
+        LocalDateTime executionWindowEnd = LocalDateTime.parse("2022-12-17T14:20:00");
+
+        List<Job> jobs = Arrays.asList(firstJob, secondJob, thirdJob);
+        ValidationResult validationResult = jobScheduler.validateJobs(executionWindowStart, executionWindowEnd, jobs);
+
+        assertFalse(validationResult.isValid());
+        assertEquals( 1, validationResult.getMessages().size());
+        assertEquals("Existem jobs com o prazo de execução fora janela de execução", validationResult.getMessages().get(0));
+    }
+
+    @Test
+    void jobs_with_duration_greather_than_8_should_be_invalid() {
+
+        Job firstJob = newJob(LocalDateTime.parse("2020-10-12T10:20:30"), Duration.ofHours(2));
+        Job secondJob = newJob(LocalDateTime.parse("2020-10-13T12:30:00"), Duration.ofHours(6));
+        Job thirdJob = newJob(LocalDateTime.parse("2020-10-12T11:20:30"), Duration.ofHours(9));
+
+        LocalDateTime executionWindowStart = LocalDateTime.parse("2020-10-12T10:20:30");
+        LocalDateTime executionWindowEnd = LocalDateTime.parse("2020-10-20T11:20:30");
+
+        List<Job> jobs = Arrays.asList(firstJob, secondJob, thirdJob);
+        ValidationResult validationResult = jobScheduler.validateJobs(executionWindowStart, executionWindowEnd, jobs);
+
+        assertFalse(validationResult.isValid());
+        assertEquals( 1, validationResult.getMessages().size());
+        assertEquals("Existem jobs com mais de 8 horas de tempo estimado", validationResult.getMessages().get(0));
+    }
+
+    @Test
+    void total_execution_duration_should_be_greather_than_execution_window_duration() {
+
+        Job firstJob = newJob(LocalDateTime.parse("2020-10-12T10:20:30"), Duration.ofHours(8));
+        Job secondJob = newJob(LocalDateTime.parse("2020-10-13T12:30:00"), Duration.ofHours(8));
+        Job thirdJob = newJob(LocalDateTime.parse("2020-10-12T11:20:30"), Duration.ofHours(8));
+
+        LocalDateTime executionWindowStart = LocalDateTime.parse("2020-10-12T10:20:30");
+        LocalDateTime executionWindowEnd = LocalDateTime.parse("2020-10-12T11:20:30");
+
+        List<Job> jobs = Arrays.asList(firstJob, secondJob, thirdJob);
+        ValidationResult validationResult = jobScheduler.validateJobs(executionWindowStart, executionWindowEnd, jobs);
+
+        assertFalse(validationResult.isValid());
+        assertEquals( 1, validationResult.getMessages().size());
+        assertEquals("O tempo de execução das jobs é maior do que a duração da janela de execução", validationResult.getMessages().get(0));
     }
 
 
