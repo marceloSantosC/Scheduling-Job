289d5fc08ee814ebbb40e1f2134cd9d3a508c3de
Marcelo dos Santos <marcelo17082000@gmail.com>
2021-12-21 23:16:51 -0300

    + Criado um front-end para o usuário + Mudada a porta padrão para 9977 + Adicionada a anotação de Cross-origin

--- src/main/java/com/marcelosantosc/scheduling/job/controller/SchedulerController.java
+++ src/main/java/com/marcelosantosc/scheduling/job/controller/SchedulerController.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/scheduler", produces = MediaType.APPLICATION_JSON_VALUE)
+@CrossOrigin
 public class SchedulerController {
 
     private final Scheduler scheduler;
